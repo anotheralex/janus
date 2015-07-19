81083304042ce5517316af1baf187f2e21e6b2c8
Alex Wollenschlaeger <alexw@panix.com>
2015-07-19 14:03:29 +0100

    recommit

---
+++ R/fit_model.R
@@ -0,0 +1,26 @@
+#' fit a model of the required type
+#' @param classifier, the classifier to use. one of: glm, e1071
+#'
+fit <- function(formula, data, classifier) {
+  switch(EXPR = classifier,
+         "glm" = fit_glm(formula, data),
+         "e1071" = fit_e1071(formula, data))
+}
+
+#' fit a logistic regression model using stats::glm
+#' @param formula, a model formula
+#' @param data, a data frame with a categorial output variable
+#'
+fit_glm <- function(formula, data) {
+  stats::glm(formula = formula,
+             data = data,
+             family = "binomial")
+}
+
+#' fit a support vector machine classifier using e1071::svm
+#' @param formula, a model formula
+#' @param data, a data frame with a categorical output variable
+#'
+fit_e1071 <- function(formula, data) {
+  print("starting e1071...")
+}
