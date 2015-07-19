4f95143935d5fcad1d18420da7102aeae700068d
Alex Wollenschlaeger <alexw@panix.com>
2015-07-19 13:31:51 +0100

    rename parameter and helper method

--- R/fit_model.R
+++ R/fit_model.R
@@ -1,18 +1,18 @@
 #' fit a model of the required type
 #' @param algorithm, one of: glm, svc
 #'
-fit <- function(formula, data, algorithm) {
-  switch(EXPR = algorithm,
-         "logistic" = stats::glm(formula = formula,
-                                 data = data,
-                                 family = "binomial"),
-         "svm" = print("running support vector machine in e1071..."))
+fit <- function(formula, data, classifier) {
+  switch(EXPR = classifier,
+         "glm" = fit_glm(formula, data),
+         "svm" = print("starting e1071..."))
 }
 
 #' fit a logistic regression model using stats::glm
+#' @param formula, a model formula
+#' @param data, a data frame with a categorial output variable
 #'
-fit_logistic_regression <- function(formula, data, ...) {
+fit_glm <- function(formula, data) {
   stats::glm(formula = formula,
-            data = data,
+             data = data,
              family = "binomial")
 }
