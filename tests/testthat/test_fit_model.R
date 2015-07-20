8c9fb31ae58e3768a919331b4f766792c48ab2ea
Alex Wollenschlaeger <alexw@panix.com>
2015-07-20 13:52:25 +0100

    test for return type

--- tests/testthat/test_fit_model.R
+++ tests/testthat/test_fit_model.R
@@ -8,3 +8,8 @@ test_that("formula parameter is not missing", {
 test_that("data parameter is not missing", {
   expect_error(fit(formula = am ~ mpg, classifier = "glm"), "missing data")
 })
+
+test_that("object of correct type returned by helper functions", {
+  model <- fit_glm(formula = am ~ mpg, data = mtcars)
+  expect_is(model, "glm")
+})
