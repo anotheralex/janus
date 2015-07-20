6fd7fae8e213547074ffcc1ef3d3ec688afe8a7e
Alex Wollenschlaeger <alexw@panix.com>
2015-07-20 13:06:32 +0100

    test for missing data

--- tests/testthat/test_fit_model.R
+++ tests/testthat/test_fit_model.R
@@ -2,5 +2,9 @@ library(janus)
 context("fitting classifier")
 
 test_that("formula parameter is not missing", {
-  expect_error(fit(classifier = "glm", "missing formula"))
+  expect_error(fit(data = mtcars, classifier = "glm"), "missing formula")
+})
+
+test_that("data parameter is not missing", {
+  expect_error(fit(formula = am ~ mpg, classifier = "glm"), "missing data")
 })
