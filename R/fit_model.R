a0c62e389e58c783507c3b6304038e8eb2a93359
Alex Wollenschlaeger <alexw@panix.com>
2015-07-21 13:40:20 +0100

    create data subset if required

--- R/fit_model.R
+++ R/fit_model.R
@@ -5,17 +5,24 @@
 #' @param data, a data frame
 #' @param classifier, a string indicating the classifier to use.
 #'    one of: glm, e1071
+#' @param subset, vector of indices for the subset to be used in fitting
 #'
-fit <- function(formula, data, classifier) {
+fit <- function(formula, data, classifier, subset) {
 
-  # check parameters
+  # check that required parameters are present and of correct class
   if(missing(formula)) {stop("missing formula", call. = FALSE)}
   if(!inherits(formula, "formula")) {
     stop("object must be a formula", call. = FALSE)
   }
+
   if(missing(data)) {stop("missing data", call. = FALSE)}
   if(!inherits(data, "data.frame")) {
     stop("data must be in a data.frame", call. = FALSE)
+  }
+
+  # create the data subset if required
+  if(!missing(subset)) {
+    data <- data[subset, ]
   }
 
   # identify correct classifier to fit and call it
