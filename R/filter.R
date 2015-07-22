16d527b0e29ab0f9866a132c4a74fe1c596a22b8
Alex Wollenschlaeger <alexw@panix.com>
2015-07-22 12:17:35 +0100

    stop if arguments are missing

--- R/filter.R
+++ R/filter.R
@@ -2,6 +2,12 @@
 #'
 #'
 
-filter <- function(formula, data) {
+filter <- function(formula, data, method) {
+
+  # test all arguments are supplied
+  if(missing(formula)) stop("Missing formula. Usage filter(formula, data, method")
+  if(missing(data)) stop("Missing data. Usage filter(formula, data, method")
+  if(missing(method)) stop("Missing data. Usage filter(formula, data, method")
+
   print("filtered features")
 }
