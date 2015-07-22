6628df9dc01aec5f1497fb67f671e684dfd4ef31
Alex Wollenschlaeger <alexw@panix.com>
2015-07-22 12:29:56 +0100

    test that missing argument raises error

--- tests/testthat/test_filter.R
+++ tests/testthat/test_filter.R
@@ -1,3 +1,15 @@
 # tests for filter functions
 library("janus")
 context("filter features")
+
+# set up example data
+y = sample(0:1, 100, replace = TRUE)
+x1 = rnorm(100)
+x2 = sample(0:1, 100, replace = TRUE)
+df <- data.frame(y, x1, x2)
+
+test_that("error is thrown if argument is missing", {
+  expect_error(filter(data = df, method = "pearson"))
+  expect_error(filter(formula = y ~ ., method = "pearson"))
+  expect_error(filter(formula = y ~ ., data = df))
+})
