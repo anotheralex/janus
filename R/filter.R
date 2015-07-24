506584d2dd79fe5700256141219d7645e5cb89d9
Alex Wollenschlaeger <alexw@panix.com>
2015-07-24 12:50:52 +0100

    provide method list as argument

--- R/filter.R
+++ R/filter.R
@@ -2,14 +2,16 @@
 #'
 #' @param formula, a formula object
 #' @param data, a data frame
-#' @param method, a string indicating the filter method to use.
-#'  Currently supported options: pearson, rank, cfs, chisq
-filter <- function(formula, data, method) {
+#' @param method, a string indicating the filter method to use
+filter <- function(formula, data, method = c("pearson", "spearman", "chisq", "cfs")) {
 
   # test all arguments are supplied
   if(missing(formula)) stop(sQuote("formula"), " is missing. Usage: filter(formula, data, method)")
   if(missing(data)) stop(sQuote("data"), " is missing. Usage: filter(formula, data, method)")
   if(missing(method)) stop(sQuote("method"), " is missing. Usage: filter(formula, data, method)")
+
+  # get full argument
+  method <- match.arg(method)
 
   # determine which filter method to use
   switch(EXPR = method,
