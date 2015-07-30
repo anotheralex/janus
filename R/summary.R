8e0207f52c0068eb652187f89c6af495c8d58bb6
Alex Wollenschlaeger <alexw@panix.com>
2015-07-30 13:49:05 +0100

    skeleton method

--- R/summary.R
+++ R/summary.R
@@ -3,5 +3,11 @@
 summary.janus <- function(object) {
   if(!is(object, "janus")) stop(sQuote("object"), "is not of class janus.")
 
-  print("janus object extends unknown class.")
+  if(is(object, "glm")) {
+    print("janus-glm object")
+  } else if(is(object, "e1071")) {
+    print("janus-e1071 object")
+  } else if(is(object, "randomForest")) {
+    print("janus-randomforest object")
+  }
 }
