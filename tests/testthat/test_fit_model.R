3fad0637c6a8ad49190068278f994441c50edc69
Alex Wollenschlaeger <alexw@panix.com>
2015-07-30 12:38:14 +0100

    test for correct return class

--- tests/testthat/test_fit_model.R
+++ tests/testthat/test_fit_model.R
@@ -17,6 +17,7 @@ test_that("error is thrown if data parameter is missing", {
 test_that("object of correct type returned by helper functions", {
   model <- .fit_glm(formula = am ~ mpg, data = mtcars)
   expect_is(model, "glm")
+  expect_is(model, "janus")
 
   model <- .fit_randomforest(formula = am ~ mpg, data = mtcars)
   expect_is(model, "randomForest")
