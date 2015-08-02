73a518269e13bf4bbeed68e5f21e23ce0585007b
Alex Wollenschlaeger <alexw@panix.com>
2015-08-02 16:09:19 +0100

    test that janus predictions match those of glm

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -7,6 +7,16 @@ test_that("error thrown object of incompatible class passed as argument", {
   expect_error(predict.janus(obj))
 })
 
+test_that("probability predictions same as that from predict.glm", {
+  jmod <- fit(am ~ mpg, data = mtcars[complete.cases(mtcars), ],
+             classifier = "glm")
+  gmod <- glm(am ~ mpg, data = mtcars[complete.cases(mtcars), ],
+              family = "binomial")
+  j_pred <- predict(jmod, type = "probability")
+  g_pred <- predict(gmod, type = "response")
+  expect_equal(j_pred, g_pred)
+})
+
 test_that("higher threshold leads to fewer 1 labels in 2-class problem", {
   mod <- fit(am ~ mpg, data = mtcars[complete.cases(mtcars), ],
              classifier = "glm")
