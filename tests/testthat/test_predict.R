aee5e0fae2e5f7731f7e81dae5941399df68f098
Alex Wollenschlaeger <alexw@panix.com>
2015-08-02 14:07:02 +0100

    check thresholding works as expected

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -6,3 +6,10 @@ test_that("error thrown object of incompatible class passed as argument", {
   obj <- lm(mpg ~ ., data = mtcars)
   expect_error(predict.janus(obj))
 })
+
+test_that("higher threshold leads to fewer 1 labels", {
+  mod <- fit(am ~ mpg, data = mtcars[complete.cases(mtcars), ], classifier = "glm")
+  low_thresh <- predict(mod, type = "class", threshold = 0.2)
+  high_thresh <- predict(mod, type = "class", threshold = 0.8)
+  expect_true(sum(low_thresh) > sum(high_thresh))
+})
