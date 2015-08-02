e140fdb03ba83bc3dbb26d43ede1ee34f1fc6b7e
Alex Wollenschlaeger <alexw@panix.com>
2015-08-02 13:50:42 +0100

    test error handling

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -1,4 +1,8 @@
 library(janus)
 context("make predictions based on fitted model")
 
-test_that()
+test_that("error thrown object of incompatible class passed as argument", {
+  # create an object of class lm
+  obj <- lm(mpg ~ ., data = mtcars)
+  expect_error(predict.janus(obj))
+})
