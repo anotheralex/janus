e99860533b2b0103eb8cc1b4d7fc6dd357480edc
Alex Wollenschlaeger <alexw@panix.com>
2015-08-02 15:09:38 +0100

    predict for new data

--- R/predict.R
+++ R/predict.R
@@ -37,19 +37,28 @@ predict.janus <- function(object,
 }
 
 # predict class labels or probabilities for a glm model
-.predict_glm <- function(object, newdata, type, threshold) {
+.predict_glm <- function(object,
+                         newdata,
+                         type,
+                         threshold = 0.5) {
+
+  # use glm::predict.glm to predict the probabilities associated with
+  # either the training data or new data
   if(missing(newdata)) {
     # predict probabilities for the training data using glm::predict.glm
     pred_probs <- predict.glm(object, type = "response")
-
-    if(type == "probability") {
-      pred_probs
-    } else if(type == "class") {
-      pred_labels <- rep(0, length(pred_probs))
-      pred_labels[pred_probs > threshold] <- 1
-      pred_labels
-    }
+  } else {
+    pred_probs <- predict.glm(object, newdata = newdata, type = "response")
   }
+
+  if(type == "probability") {
+    pred_probs
+  } else if(type == "class") {
+    pred_labels <- rep(0, length(pred_probs))
+    pred_labels[pred_probs > threshold] <- 1
+    pred_labels
+  }
+
 }
 
 .predict_e1071 <- function(object, newdata, type) {
