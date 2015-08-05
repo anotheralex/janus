5754f860434cd611d91dd8a5043ed555779344b3
Alex Wollenschlaeger <alexw@panix.com>
2015-08-05 12:15:43 +0100

    update documentation

--- R/fit_model.R
+++ R/fit_model.R
@@ -43,10 +43,18 @@ fit <- function(formula, data, classifier = c("e1071", "glm", "randomforest"),
 
 }
 
-#' fit a support vector machine classifier using e1071::svm
+#' fit a support vector machine classifier
+#'
+#' Uses e1071::svm to fit a support vector machine. Note that the type
+#' argument to svm is set to force classification. Also, the function uses
+#' linear as the default to the kernel argument rather than radial as in native
+#' svm. The cost parameter retains its default (i.e. cost = 1) and can be
+#' modified by passing through a different value.
+#'
 #' @param formula, a model formula
 #' @param data, a data frame with a categorical output variable
 #' @param ... arguments to pass to called functions
+#'
 #' @return fitted model in object of class janus
 #'
 .fit_e1071 <- function(formula, data, ...) {
