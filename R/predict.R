4a6e363646d898a165f3e2c003d4eab8d79bad7b
Alex Wollenschlaeger <alexw@panix.com>
2015-08-06 14:16:09 +0100

    first pass at e1071 predictions
    Note that this is working for class prediction but not currently for probability prediction. Also, it is currently necessary to pass in all data, even when making predictions on the training data, since this is not immediately accessible from the svm object

--- R/predict.R
+++ R/predict.R
@@ -65,8 +65,43 @@ predict.janus <- function(object,
 }
 
 .predict_e1071 <- function(object, newdata, type, ...) {
-  # TODO note that this requires a newdata object, and the old data is NOT
-  # passed through
+
+  #loadNamespace("e1071")
+
+  # set the class of the object to that of e1071 svm object
+  # required to gain access to the correct predict function
+  class(object) <- c("svm.formula", "svm")
+
+  # use e1071::predict.svm to predict the probabilities associated with
+  # either the training data or new data
+  # note that there is a fair amount of code duplication here but it is
+  # necessary to prevent the copying of potentially large data objects
+  if(is.null(newdata)) {
+
+    if(type == "probability") {
+      # predict probabilities for the training data
+      # need to identify the data from the fitted model object
+      pred_probs <- predict(object,
+                            quote(object$call$data),
+                            decision.values = TRUE,
+                            probability = TRUE)
+      attr(pred_probs, "probalities")
+    } else if(type == "class") {
+      pred_labels <- predict(object, quote(object$call$data))
+      pred_labels
+    }
+  } else {
+    if(type == "probability") {
+      pred_probs <- predict(object,
+                            newdata,
+                            decision.values = TRUE,
+                            probability = TRUE)
+      attr(pred_prob, "probabilities")
+    } else if(type == "class") {
+      pred_labels <- predict(object, newdata)
+      pred_labels
+    }
+  }
 }
 
 # predict class labels or probabilities for a randomForest model
