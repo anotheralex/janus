f83348f0a7c64d7aa4c901c66421f6c76fde9312
Alex Wollenschlaeger <alexw@panix.com>
2015-08-07 21:07:00 +0100

    test predict randomforest comparable with base package

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -17,6 +17,21 @@ test_that("probability predictions same as that from predict.glm", {
   expect_equal(j_pred, g_pred)
 })
 
+test_that("probability predictions same as that from predict.randomForest", {
+  mt_complete <- mtcars[complete.cases(mtcars), ]
+
+  library("randomForest")
+  set.seed(1)
+  r_mod <- randomForest(as.factor(am) ~ ., data = mt_complete)
+  r_pred <- predict(r_mod, type = "prob")
+
+  set.seed(1)
+  j_mod <- fit(as.factor(am) ~ ., data = mt_complete,
+               classifier = "randomforest")
+  j_pred <- predict(j_mod, type = "probability")
+  expect_equal(j_pred, r_pred)
+})
+
 test_that("higher threshold leads to fewer 1 labels in 2-class glm problem", {
   mod <- fit(am ~ mpg, data = mtcars[complete.cases(mtcars), ],
              classifier = "glm")
