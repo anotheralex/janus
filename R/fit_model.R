2986859f80db04b518c296eebca28a465397a9f5
Alex Wollenschlaeger <alexw@panix.com>
2015-08-11 12:54:59 +0100

    check arguments and call glmnet to train model

--- R/fit_model.R
+++ R/fit_model.R
@@ -107,8 +107,9 @@ fit.formula <- function(formula,
 #' fit a binomial or multinomial logistic regression classifier using
 #'    glmnet::glmnet
 #'
-#' @param formula a model formula
-#' @param data a data frame with a categorical output variable
+#' @param x a dataframe or matrix containing predictors
+#' @param y a factorn vector containing response variables with two or more
+#'    levels
 #' @param ... additional parameters to pass to glmnet
 #'
 #' @return fitted model in object of class janus
@@ -116,8 +117,43 @@ fit.formula <- function(formula,
 #' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
 #'
 #' @export
-.fit_glmnet <- function(formula, data, ...) {
-  model <- glmnet::glmnet(formula = formula, data = data, ...)
+.fit_glmnet <- function(x, y, ...) {
+
+  # check that necessary data has been supplied and in correct form
+  if(missing(x)) stop(sQuote("x"), " missing")
+  if(missing(y)) stop(sQuote("y"), " missing")
+  if(is.null(x)) stop(sQuote("x"), " cannot be null")
+  if(is.null(y)) stop(sQuote("y"), " cannot be null")
+
+  # check if y is a factor variable and if not warn that it will be coerced
+  if(!is.factor(y)) {
+    message(sQuote("y"), " is not a factor and will be coerced")
+  }
+
+  # check how many levels the factor response variable has
+  if(is.factor(y)) {
+    levels <- nlevels(y)
+  } else {
+    num_levels <- length(unique(y))
+    if (num_levels == length(y)) {
+      stop(sQuote("y"), " is not a valid factor variable")
+    } else {
+      levels <- length(unique(y))
+    }
+  }
+
+  # fit a bi- or multinomial model based on the number of levels in the
+  # response variable
+  if(levels == 2) {
+    model <- glmnet::glmnet(x, y, family = "binomial")
+  } else if (levels > 2) {
+    model <- glmnet::glmnet(x, y, family = "multinomial",
+                            type.multinomial = "grouped")
+  } else {
+    stop("Unsupported number of levels in factor variable ", sQuote("y"))
+  }
+
+  model <- glmnet::glmnet(x, y, ...)
   class(model) <- c("janus", class(model))
   model
 }
