60b244f7460019baf73367547485b5bb37c1a33f
Alex Wollenschlaeger <alexw@panix.com>
2015-08-13 12:59:05 +0100

    add a logical to confirm that constructor was used
    Some janus objects are created by simply extending the class of the underlying model object, while constructed janus objects contain the unaltered underlying model object as a field.

--- R/janus.R
+++ R/janus.R
@@ -4,6 +4,8 @@
 #' @param package character string indicating package origin of classifier
 #' @param classifier character string indicating the classifier used to train
 #'    the model in object
+#' @param constructed logical indicating whether this object was created using
+#'    the janus constructor
 #'
 #' @return a janus object containing the trained model object with additional
 #'    metadata
@@ -12,11 +14,12 @@
 #'
 #' @export
 janus <- function(object, package, classifier,
-                  interface = c("formula", "default")) {
+                  interface = c("formula", "default"), constructed = TRUE) {
   structure(list(model = object,
                  package = package,
                  classifier = classifier,
-                 interface = interface),
+                 interface = interface,
+                 constructed = constructed),
             class = c("janus", class(object))
   )
 }
