d11c6afdfd761108eb4e8d51c3b86dbf2552685c
Alex Wollenschlaeger <alexw@panix.com>
2015-08-13 12:39:59 +0100

    expand metadata

--- R/janus.R
+++ R/janus.R
@@ -1,17 +1,22 @@
 #' constructor for janus object
 #'
-#' @param obj a trained model object
+#' @param object a trained model object
+#' @param package character string indicating package origin of classifier
+#' @param classifier character string indicating the classifier used to train
+#'    the model in object
 #'
 #' @return a janus object containing the trained model object with additional
-#'    metadata extracted from the object
+#'    metadata
 #'
 #' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
 #'
 #' @export
-janus <- function(obj) {
-  structure(list(model = obj,
-                 formula = obj$call$formula,
-                 data = obj$call$data),
-            class = c("janus", class(obj))
+janus <- function(object, package, classifier,
+                  interface = c("formula", "default")) {
+  structure(list(model = object,
+                 package = package,
+                 classifier = classifier,
+                 interface = interface),
+            class = c("janus", class(object))
   )
 }
