63498f22ae6ce9d61c48380b56819fb534318082
Alex Wollenschlaeger <alexw@panix.com>
2015-08-14 22:18:34 +0100

    update documentation

--- R/fit_randomforest.R
+++ R/fit_randomforest.R
@@ -1,8 +1,14 @@
-#' fit a support vector machine classifier using randomForest::randomForest
-#' @param formula, a model formula
-#' @param data, a data frame with a categorical output variable
-#' @return fitted model in object of class janus
+#' Fit a support vector machine classifier using randomForest::randomForest
 #'
+#' @param formula A formula object.
+#' @param data A data frame containing the predictor variables and a categorical
+#'    response variable.
+#'
+#' @return A fitted model in object of class janus.
+#'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 fit_randomforest <- function(formula, data) {
   model <- randomForest::randomForest(formula = formula, data = data)
   class(model) <- c("janus", class(model))
