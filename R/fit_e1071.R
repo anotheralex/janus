ecc58dfffc3ebb4a8dd26cf41b1a501503d22faa
Alex Wollenschlaeger <alexw@panix.com>
2015-08-14 22:18:04 +0100

    update documentation

--- R/fit_e1071.R
+++ R/fit_e1071.R
@@ -1,19 +1,22 @@
-#' fit a support vector machine classifier
+#' Fit a support vector machine classifier using e1071::svm.
 #'
-#' Uses e1071::svm to fit a support vector machine. Note that the type
+#' Uses svm from the e1071 package to fit a support vector machine. The type
 #' argument to svm is set to force classification. Also, the function uses
 #' linear as the default to the kernel argument rather than radial as in native
 #' svm. The cost parameter retains its default (i.e. cost = 1) and can be
 #' modified by passing through a different value.
 #'
-#' @param formula, a model formula
-#' @param data, a data frame with a categorical output variable
-#' @param ... arguments to pass to called functions
+#' @param formula A formula object.
+#' @param data A dataframe containing the predictor variables and a categorical
+#'    response variable.
+#' @param ... Arguments to pass to called functions.
 #'
-#' @return fitted model in object of class janus
+#' @return A fitted model in object of class janus.
 #'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 fit_e1071 <- function(formula, data, ...) {
-  # TODO: check documentation for requirements to default to classification
   # response variable needs to be a factor to give classification
   # note that default kernel is linear not radial as in e1071::svm
   model <- e1071::svm(formula = formula,
