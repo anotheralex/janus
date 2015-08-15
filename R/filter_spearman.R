72a6bfe50f1eaa8cd8dc2019ce477a25293e13c1
Alex Wollenschlaeger <alexw@panix.com>
2015-08-15 13:26:24 +0100

    update documentation

--- R/filter_spearman.R
+++ R/filter_spearman.R
@@ -1,6 +1,17 @@
-#' Univariate filter using Spearman's rank correlation with output variable
-#' @param formula, a formula object
-#' @param data, a data frame
+#' Univariate filter using Spearman rank correlation.
+#'
+#' Uses Spearman's rank correlation for feature selection, as implemented in the
+#' FSelector package, to select important features.
+#'
+#' @param formula A formula object.
+#' @param data A dataframe containing predictor and response variables.
+#' @param limit Optional integer specifying the number of features to retain.
+#'
+#' @return A vector of feature importance values.
+#'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 filter_spearman <- function(formula, data, limit) {
   result <- FSelector::rank.correlation(formula, data)
   result <- result[order(result, decreasing = TRUE), , drop = FALSE]
