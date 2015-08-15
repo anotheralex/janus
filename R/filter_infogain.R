0c1ff734bcac9b5c5bf9f76f32dfd89644e64b00
Alex Wollenschlaeger <alexw@panix.com>
2015-08-15 13:23:58 +0100

    update documentation

--- R/filter_infogain.R
+++ R/filter_infogain.R
@@ -1,6 +1,17 @@
-#' Univariate filter using Information Gain
-#' @param formula, a formula object
-#' @param data, a data frame
+#' Univariate filter using information gain.
+#'
+#' Uses information gain for feature selection, as implemented in the FSelector
+#' package, to select important features.
+#'
+#' @param formula A formula object.
+#' @param data A dataframe containing predictor and response variables.
+#' @param limit Optional integer specifying the number of features to retain.
+#'
+#' @return A vector of feature importance values.
+#'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 filter_infogain <- function(formula, data, limit) {
   result <- FSelector::information.gain(formula, data)
   result <- result[order(result, decreasing = TRUE), , drop = FALSE]
