bdae1999b5d33f82f93e705484df8ecc11f53534
Alex Wollenschlaeger <alexw@panix.com>
2015-08-15 13:25:15 +0100

    update documentation

--- R/filter_chisq.R
+++ R/filter_chisq.R
@@ -1,6 +1,17 @@
-#' Univariate filter using Chi-squared
-#' @param formula, a formula object
-#' @param data, a data frame
+#' Univariate filter using chi-squared.
+#'
+#' Uses $$\chi^2$$ for feature selection, as implemented in the FSelector
+#' package, to select important features.
+#'
+#' @param formula A formula object.
+#' @param data A dataframe containing predictor and response variables.
+#' @param limit Optional integer specifying the number of features to retain.
+#'
+#' @return A vector of feature importance values.
+#'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 filter_chisq <- function(formula, data, limit) {
   result <- FSelector::chi.squared(formula, data)
   result <- result[order(result, decreasing = TRUE), , drop = FALSE]
