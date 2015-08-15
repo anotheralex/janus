32682eb28d7361d5f6cc5dca8dfcb708e61fca44
Alex Wollenschlaeger <alexw@panix.com>
2015-08-15 13:19:05 +0100

    update documentation

--- R/filter_cfs.R
+++ R/filter_cfs.R
@@ -1,6 +1,13 @@
-#' Feature selection using the CFS algorithm
-#' @param formula, a formula object
-#' @param data, a data frame
+#' Feature selection using CFS algorithm
+#'
+#' @param formula A formula object.
+#' @param data A dataframe containing predictor and response variables.
+#'
+#' @return A vector of predictor identifiers.
+#'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 filter_cfs <- function(formula, data) {
   FSelector::cfs(formula, data)
 }
