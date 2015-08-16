0b8aa87a2117d0bad63404d13572a193ab766393
Alex Wollenschlaeger <alexw@panix.com>
2015-08-16 15:28:53 +0100

    update arguments

--- R/plot.R
+++ R/plot.R
@@ -1,13 +1,14 @@
 #' Plot a janus object.
 #'
 #' @param object A janus object containing a fitted model.
+#' @param ... Additional plotting parameters.
 #'
 #' @return Nothing
 #'
 #' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
 #'
 #' @export
-plot.janus <- function(object) {
+plot.janus <- function(object, ...) {
   if(!inherits(object, "janus")) stop(sQuote("object"), " is not compatible.")
 
   if(inherits(object, "glmnet") || inherits(object, "cv.glmnet")) {
--- R/plot_glmnet.R
+++ R/plot_glmnet.R
@@ -1,13 +1,14 @@
 #' Plot a janus object derived from a glmnet or cv.glmnet object
 #'
-#' @param object a janus object containing a fitted model
+#' @param object A janus object containing a fitted model.
+#' @param ... Additional plotting parameters.
 #'
-#' @return nothing
+#' @return Nothing
 #'
 #' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
 #'
 #' @export
-plot_glmnet <- function(object) {
+plot_glmnet <- function(object, ...) {
   if(!inherits(object, "glmnet") && !inherits(object, "cv.glmnet")) {
     stop(sQuote("object"), " is not compatible with this method.")
   }
