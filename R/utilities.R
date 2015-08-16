bb500e58651732c123e0d87b0d5a606126f3a194
Alex Wollenschlaeger <alexw@panix.com>
2015-08-16 14:02:05 +0100

    update documentation

--- R/utilities.R
+++ R/utilities.R
@@ -1,18 +1,24 @@
-#' Generic method for expanding the formula associated with a model object
+#' Generic method for expanding the formula associated with a model object.
+#'
+#' @param model A fitted model constructed using a formula interface.
+#'
+#' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
+#'
+#' @export
 expand_formula <- function(model) {
   UseMethod("expand_formula")
 }
 
-#' Expand the formula associated with a janus fitted model object
+#' Expand the formula associated with a janus fitted model object.
 #'
-#' If a formula is passed to fit in a variable, the variable will be stored
-#'    in the resulting fitted model object's call variable. This method is a
-#'    convenient utility for extracting and expanding the formula from a fitted
-#'    janus model object.
+#' If a formula is passed to fit in a variable, the variable will be stored in
+#' the resulting fitted model object's call variable. This method is a
+#' convenient utility for extracting and expanding the formula from a fitted
+#' janus model object.
 #'
-#' @param model a janus fitted model
+#' @param model A fitted model in an object of class janus.
 #'
-#' @return a formula object containing the expanded formula
+#' @return A formula object containing the expanded formula.
 #'
 #' @author Alex Wollenschlaeger, \email{alexw@@panix.com}
 #'
